00526a7723304e2294d1e7f408703a4f0194d85f
Nathali Motooka <nanimotooka@gmail.com>
2023-01-03 19:53:02 -0300

    first steps

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Action1 } from './components/action1/action1'
+
+
+export const App = () => {
+  return(
+    <main>
+      <Action1 />
+    </main>
+  )
+}
+
---
+++ vite-project/src/components/action1/action1.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import styles from "./action1.module.scss";
+
+export const Action1 = () => {
+  return (
+    <section className={styles.Section}>
+      <div className={styles.row}>
+          <h2>The <span>#1</span> source for children's music</h2>
+          <p>An ever-growing catalogue of high-quality children's music, sourced by talented music creators from all over the world</p>
+        </div>
+
+        <div className={styles.column} >
+         
+        </div>
+    </section>
+  );
+};
